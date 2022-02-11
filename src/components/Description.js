52d21c352b7a9e56c34c090f1155494c352308ef
Sumit Kumar <jnvsumit@gmail.com>
2022-02-11 20:23:59 +0530

    [FIX]: Route

--- src/components/Description.js
+++ src/components/Description.js
@@ -8,15 +8,13 @@ const Description = (props) => {
     return ( 
         <>
         <div className="jumbotron jumbotron-fluid my-5 p-4" style={style}>
-            <div className="row">
-                <div className="col-lg-4">
-                    <img className="card-img-top" src={image.src} alt={image.alt}/>
-                </div>
-                <div className="col-lg-8">
-                    <div className="container py-3">
-                        <a className="display-6 text-dark" href="#" style={{textDecoration: "none"}}>{title}</a>
-                        <p className="lead">{desc}</p>
-                    </div>
+            <div className="ro">
+                <img className="card-img-top" src={image.src} alt={image.alt}/>
+            </div>
+            <div className="co">
+                <div className="container py-3">
+                    <a className="display-6 text-dark" href="#" style={{textDecoration: "none"}}>{title}</a>
+                    <p className="lead">{desc}</p>
                 </div>
             </div>
             <div className="py-3 text-center">
